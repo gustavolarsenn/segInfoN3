3cfcc4de7e0fd597b2d3093d0da309bb5ee00a86
femondinii <femondini20@gmail.com>
2024-07-03 19:37:07 -0300

    tela cadastro

--- seginfo/src/app/components/navbar.jsx
+++ seginfo/src/app/components/navbar.jsx
@@ -2,21 +2,32 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div>
-      <h1>Sistema de despesas</h1>
-      <nav>
-        <ul>
+    <div className="navbar text-white bg-lightblue">
+      <Link href="/dashboard" className="btn btn-ghost text-xl">
+        Sistema de despesas
+      </Link>
+        <ul
+          tabIndex={0}
+          className="menu center-menu bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+        >
           <li>
             <Link href="/dashboard">Home</Link>
           </li>
           <li>
-            <Link href="/register">Cadastro</Link>
+            <a>Cadastro</a>
+            <ul className="p-2">
+              <li>
+                <Link href="/employee">Funcionário</Link>
+              </li>
+              <li>
+                <Link href="/register">Despesa</Link>
+              </li>
+            </ul>
           </li>
           <li>
-            <Link href="/signature">Assinaturas</Link>
+            <Link href="/signature">Assinatura</Link>
           </li>
         </ul>
-      </nav>
     </div>
   );
 }
